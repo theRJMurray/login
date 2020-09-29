47e5198a7c0f00257d76b1d862e425fb53ac8058
therjmurray <therjmurray@gmail.com>
2020-09-28 23:40:39 -0400

    login and password console.logs input fields

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { container } from './styles'
 
 const Login = () => {
+	const [username, setUsername] = useState("")
+	const [password, setPassword] = useState("")
 
+	const handleUsernameUpdate = input => setUsername(input)
+	const handlePasswordUpdate = input => setPassword(input)
+
+	return <div style={container}>
+		<div style={{ width: 500, margin: '80px 0 0 30px', display: 'flex', flexDirection: 'column' }}>
+			<form style={{display: 'flex', flexDirection: 'column'}}>
+				<p>Username:</p>
+				<input type="text" name="username" style={{width: 200, marginTop: 20}} onChange={e => handleUsernameUpdate(e.target.value)} />
+
+				<p>Password:</p>
+				<input type="text" name="password" style={{width: 200, marginTop: 20}} onChange={e => handlePasswordUpdate(e.target.value)} />
+
+				<input type="submit" value="Submit" style={{width: 200, marginTop: 20}} onClick={e =>{
+					e.preventDefault()
+					console.log(username, password)
+				}}/>
+			</form>
+		</div>
 	</div>
 }
 
