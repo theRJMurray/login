499e7eefe43c06c82b1693ce95288927bed5a030
therjmurray <therjmurray@gmail.com>
2020-09-28 23:40:04 -0400

    move verbose styles to separate file

--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,6 @@
 import React from 'react'
 
 const Login = () => {
-	return <div style={{
-		position: 'absolute',
-		top: '50%',
-		left: '50%',
-		background: '#FFF',
-		width: 600,
-		height: 400,
-		transform: 'translate(-50%, -50%)'
-	}}>
-		<div style={{
-			margin: '80px 0 0 30px'
-		}}>Username:</div>
 
 	</div>
 }
---
+++ src/styles.js
@@ -0,0 +1,9 @@
+export const container = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    background: '#FFF',
+    width: 600,
+    height: 400,
+    transform: 'translate(-50%, -50%)'
+}
